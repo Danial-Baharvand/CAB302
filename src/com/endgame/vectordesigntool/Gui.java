36a532e76a2f9ce98beeb7fb6055d0c29ee40a1c
Danial Baharvand <dbdpcg@gmail.com>
2019-05-22 03:52:44 +1000

    A really barebones gui. The floating windows don't resise with the window yet. but the history box is pretty good for our purpose.
    Signed-off-by: Danial Baharvand <dbdpcg@gmail.com>

--- src/com/endgame/vectordesigntool/Gui.java
+++ src/com/endgame/vectordesigntool/Gui.java
@@ -5,10 +5,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.awt.Color.black;
+
 /**
  *
  * @authors Group_010 - Daniel Baharvand, James Dick, Jai Hunt, Jovi Lee
- * @version 1.0
+ * @version 1.1
  */
 public class Gui extends JFrame implements ActionListener, Runnable {
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -29,8 +31,71 @@ public class Gui extends JFrame implements ActionListener, Runnable {
     private void createGUI(){
         setSize((int)(WIDTH * 0.85), (int)(HEIGHT * 0.85));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        setLayout(null);
         setVisible(true);
+        //Make the color chooser
+            //Make the floating windows
+        JInternalFrame colorWindow = new JInternalFrame("Color");
+        colorWindow.setSize(600, 250);
+        colorWindow.setLocation((int)WIDTH-1000, (int)HEIGHT-500);
+        colorWindow.setVisible(true);
+            //Make the color chooser and add it to the window
+        JColorChooser colors= new JColorChooser(black);
+        colors.setPreviewPanel(new JPanel());
+        colorWindow.add(colors);
+        add(colorWindow);
+        //Make shapes controls
+        JInternalFrame shapesWindow = new JInternalFrame("Shapes");
+        shapesWindow.setSize(100, 500);
+        shapesWindow.setLocation(0, 30);
+        shapesWindow.setVisible(true);
+        JPanel shapesPanel = new JPanel(new GridLayout(4, 1));
+        JButton lineButton = new JButton("LINE" );
+        shapesPanel.add(lineButton);
+        JButton recButton = new JButton("RECTANGLE" );
+        shapesPanel.add(recButton);
+        JButton elButton = new JButton("ELLIPSE" );
+        shapesPanel.add(elButton);
+        JButton polButton = new JButton("POLYGON" );
+        shapesPanel.add(polButton);
+        shapesWindow.add(shapesPanel);
+        add(shapesWindow);
+        //Create a white canvas
+        JPanel canvas = new JPanel();
+        canvas.setSize(1000, 1000);
+        canvas.setLocation(150, 50);
+        canvas.setOpaque(true);
+        canvas.setBackground(Color.WHITE);
+        add(canvas);
+        //Create menu
+        JMenuBar cyanMenuBar = new JMenuBar();
+        cyanMenuBar.setOpaque(true);
+        cyanMenuBar.setBackground(Color.cyan);
+        cyanMenuBar.setPreferredSize(new Dimension(200, 20));
+        JMenu menuOpen = new JMenu("Open");
+        JMenu menuSave = new JMenu("save");
+        cyanMenuBar.add(menuOpen);
+        cyanMenuBar.add(menuSave);
+        setJMenuBar(cyanMenuBar);
+        //History
+        String subject[] = { "shapesPanel.add(elButton)", " shapesPanel.add(recButto",
+                "hapesWindow.setSize(100, ", "   apesWindow.add(shapesPanel);", "   menuOpen = new JMenu(\"Open\" ",
+                " orChooser colors= new JColorChooser(bla ",
+                " Bar.setPreferredSize(new Dimen ", "Frame shapesWindow = new JInternalFram" };
+        JInternalFrame historyWindow = new JInternalFrame("History");
+        JList<String> list = new JList<String>(subject);
+        JScrollPane scrollWindow = new JScrollPane(list);
+        scrollWindow.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollWindow.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        //historyWindow.getRootPane().setWindowDecorationStyle(JRootPane.PLAIN_DIALOG);
+        historyWindow.add(scrollWindow);
+        historyWindow.setSize(300, 300);
+        historyWindow.setLocation(1500, 0);
+        historyWindow.setVisible(true);
+        add(historyWindow);
+
+
     }
 
     @Override
