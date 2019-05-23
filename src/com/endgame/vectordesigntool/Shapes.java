ee34f65f8e4034cfb1d8b5c6411e7bf52db1b7b8
Danial Baharvand <dbdpcg@gmail.com>
2019-05-23 19:35:14 +1000

    Ver 1.5 Complete shapes functionality. The code is very messy and varriables are all over the place. needs to be refactored.
    Signed-off-by: Danial Baharvand <dbdpcg@gmail.com>

---
+++ src/com/endgame/vectordesigntool/Shapes.java
@@ -0,0 +1,63 @@
+package com.endgame.vectordesigntool;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+class Shapes {
+    static int pressedX=-1;
+    static int pressedY=-1;
+    static ArrayList<Integer> polX = new ArrayList<Integer>();
+    static ArrayList<Integer> polY = new ArrayList<Integer>();
+    static int polCount=1;
+    static int polLastX=-1;
+    static int polLastY=-1;
+    static Graphics g = Gui.canvas.getGraphics();
+    static void plot(int x,int y) {
+        g.drawLine(x,y,x,y);
+    }
+    static void line(int x,int y) {
+        if (pressedX < 0){pressedX=x;pressedY=y;}
+        else {
+            g.drawLine(pressedX, pressedY, x, y);
+            pressedX=-1;
+        }
+    }
+    static void rect(int x,int y) {
+        if (pressedX < 0){pressedX=x;pressedY=y;}
+        else {
+            g.drawRect(pressedX, pressedY, x-pressedX, y-pressedY);
+            pressedX=-1;
+        }
+    }
+    static void ellipse(int x,int y) {
+        if (pressedX < 0){pressedX=x;pressedY=y;}
+        else {
+            g.drawOval(pressedX, pressedY, x-pressedX, y-pressedY);
+            pressedX=-1;
+        }
+    }
+    static void polygon(int x,int y) {
+        if (pressedX < 0){
+            pressedX=x;
+            pressedY=y;
+            polLastX=x;
+            polLastY=y;
+            polX.add(x);polY.add(y);
+        }
+        else if (x==pressedX &&y==pressedY){
+            g.drawPolygon(polX.stream().mapToInt(Integer::intValue).toArray(),polY.stream().mapToInt(Integer::intValue).toArray(),polCount);
+            polX.clear();
+            polY.clear();
+            pressedX=-1;
+            polCount=1;
+        }else{
+            g.drawLine(polLastX, polLastY, x, y);
+            polX.add(x);
+            polY.add(y);
+            polLastX=x;
+            polLastY=y;
+            polCount++;
+        }
+    }
+}
